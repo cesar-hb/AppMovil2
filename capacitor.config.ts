8c66a67422a88b27de3faa558d6acd6af2db591e
CesarHB <chenriquezbachmann@gmail.com>
2023-10-28 14:47:40 -0300

    Commit inicial

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,30 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  // CGV-INI: Se debe agregar el soporte de SQLite para poder usar el plugin en Android
+  plugins: {
+    CapacitorSQLite: {
+    iosDatabaseLocation: 'Library/CapacitorDatabase',
+    iosIsEncryption: false,
+    iosKeychainPrefix: 'AppMovil2',
+    iosBiometric: {
+        biometricAuth: false,
+        biometricTitle : "Biometric login for capacitor sqlite"
+    },
+    androidIsEncryption: false,
+    androidBiometric: {
+        biometricAuth : false,
+        biometricTitle : "Biometric login for capacitor sqlite",
+        biometricSubTitle : "Log in using your biometric"
+    },
+    electronIsEncryption: false,
+    electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+    electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
+    electronLinuxLocation: "Databases"
+    }
   }
+  // CGV-FIN
 };
 
 export default config;
